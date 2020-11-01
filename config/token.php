dabac3bcdc85faec9ff8398092f3b032e42bbd49
EC2 Default User <ec2-user@ip-172-31-39-69.ap-northeast-1.compute.internal>
2020-11-01 20:17:40 +0900

    master

--- config/token.php
+++ config/token.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+    'LINE_ACCESS_TOKEN' => env('LINE_ACCESS_TOKEN'),
+    'LINE_CHANNEL_SECRET' => env('LINE_CHANNEL_SECRET'),
+    'LINE_API_KEY' => env('LINE_API_KEY'),
+    'WEATHER_API_KEY' => env('WEATHER_API_KEY'),
     'ANICT_API' => env('ANICT_API'),
 ];
