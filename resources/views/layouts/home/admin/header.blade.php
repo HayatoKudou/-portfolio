2a75a1699c607797556fc58d8c8de08700f4b8d4
EC2 Default User <kudoh115@gmail.com>
2020-11-23 17:05:54 +0900

    master

--- resources/views/layouts/home/admin/header.blade.php
+++ resources/views/layouts/home/admin/header.blade.php
@@ -1,5 +1,39 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>駆け出しエンジニアの開発ブログ</title>
+        <link rel="icon" href="{{ asset('img/favicon.ico') }}">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Scripts -->
+        <!-- <script src="/js/jquery.min.js"></script> -->
+        <script src="/js/app.js"></script>
+        <script src="/js/app.js" defer></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+        <!--アイコン-->
+        <script src="https://kit.fontawesome.com/4146f9dc55.js" crossorigin="anonymous"></script>
+
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="/css/app.css" rel="stylesheet">
+        <link href="/css/common.css" rel="stylesheet">
+        <link href="/css/home/main.css" rel="stylesheet">
+        <link href="/css/home/header.css" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="{{ asset('/js/loading.js') }}"></script>
+
+    </head>
 <body>
     <header>
         <div class="container">
