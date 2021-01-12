c2863a967ca5f6ee83c80fca1d6c39fd70ae208a
kudoh115@gmail.com <kudoh115@gmail.com>
2021-01-12 21:54:54 +0900

    regular_expression

--- resources/views/layouts/tools/anime/anime.blade.php
+++ resources/views/layouts/tools/anime/anime.blade.php
@@ -6,6 +6,6 @@
 
 
 <!-- <main class="py-4"> -->
-<main class="py-4" id="main_content" style="padding: 70px 0 0 0!important;">
+<main class="py-4" id="main_content" class="main_content">
     @yield('content')
 </main>
